76fabc0cb4b1e056f4396b7ea4a22a618aece019
poojithaprakash <poojitha.19cs118@cambridge.edu.in>
2022-12-31 16:22:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import './App.css';
+// import { connect } from 'react-redux';
+// import { IncAction } from './actions';
+// import { DecAction } from './actions';
+
+// const App=({local_variable, IncAction,DecAction}) => {
+//   return (
+//     <div>
+//       <center>
+//         <h1>{local_variable}</h1>
+//         <button onClick={IncAction}>Increment</button>
+//         <button onClick={DecAction}>Decrement</button>
+//       </center>
+//     </div>
+//   )
+// }
+
+// const mapStateToProps = state =>({
+//   local_variable: state
+// })
+
+// export default connect(mapStateToProps, {IncAction, DecAction})(App);
+
+
+import Counter from "./features/counter/Counter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <Counter />
+    </main>
   );
 }
 
